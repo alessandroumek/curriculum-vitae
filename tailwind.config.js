f41258b7820618ded282f9b1d974a66d3647365d
Alessandro Umek <alessandro.umek@telit.com>
2024-11-05 14:37:21 +0100

    configured tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      foreground : {
+        500: '#9A9A9A',
+        900: '#000000'
+      },
+      background: {
+        white: '#FFFFFF'
+      }
+    },
     extend: {},
   },
   plugins: [],
